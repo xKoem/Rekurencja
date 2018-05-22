fed974ecfbb89b9082c7f306aff53b1fd01a8172
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 11:38:16 +0200

    countHi

---
+++ src/main/java/pl/xkoem/CountHi.java
@@ -0,0 +1,13 @@
+package pl.xkoem;
+
+public class CountHi {
+    public int countHi(String str) {
+        if (str.length() < 2) {
+            return 0;
+        }
+        if (str.charAt(str.length()-1) == 'i' && str.charAt(str.length()-2) == 'h') {
+            return 1 + countHi(str.substring(0, str.length()-2));
+        }
+        return countHi(str.substring(0, str.length()-1));
+    }
+}
