a87fdbefa475dafd259d725bb9313e39c9186998
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 14:25:01 +0200

    pairStar

---
+++ src/main/java/pl/xkoem/PairStar.java
@@ -0,0 +1,17 @@
+package pl.xkoem;
+
+public class PairStar {
+    public String pairStar(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        String newString;
+        if (str.charAt(str.length()-2) == str.charAt(str.length() - 1)) {
+            newString = "*" + str.charAt(str.length() - 1);
+        } else {
+            newString = "" + str.charAt(str.length() - 1);
+        }
+
+        return pairStar(str.substring(0, str.length() -1)) + newString;
+    }
+}
