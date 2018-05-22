a373cca5dfe49990addbf0c1974d21159da63517
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 12:28:35 +0200

    array220

---
+++ src/main/java/pl/xkoem/Array220.java
@@ -0,0 +1,13 @@
+package pl.xkoem;
+
+public class Array220 {
+    public boolean array220(int[] nums, int index) {
+        if (index > nums.length-2) {
+            return false;
+        }
+        if (nums[index+1] == nums[index] * 10) {
+            return true;
+        }
+        return array220(nums, index + 1);
+    }
+}
