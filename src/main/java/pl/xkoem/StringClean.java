6a692ab1c0e73fccb31ab855ce8914ea9828be68
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 15:41:36 +0200

    stringClean

---
+++ src/main/java/pl/xkoem/StringClean.java
@@ -0,0 +1,13 @@
+package pl.xkoem;
+
+public class StringClean {
+    public String stringClean(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        if (str.charAt(0) == str.charAt(1)) {
+            return stringClean(str.substring(1));
+        }
+        return str.charAt(0) + stringClean(str.substring(1));
+    }
+}
