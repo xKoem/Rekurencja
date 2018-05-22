f671e3777fd380e2f7e82d4d2d8409445c19c0a3
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 11:59:52 +0200

    changePi

---
+++ src/main/java/pl/xkoem/ChangePi.java
@@ -0,0 +1,13 @@
+package pl.xkoem;
+
+public class ChangePi {
+    public String changePi(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+        if (str.charAt(str.length()-1) == 'i' && str.charAt(str.length()-2) == 'p') {
+            return changePi(str.substring(0, str.length()-2)) + "3.14";
+        }
+        return changePi(str.substring(0, str.length()-1)) + str.charAt(str.length()-1);
+    }
+}
