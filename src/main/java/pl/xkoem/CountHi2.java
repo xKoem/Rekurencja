faa1dc13909ec3656ff9b9bc118dbe18679aa9a2
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 15:52:52 +0200

    countHi2

---
+++ src/main/java/pl/xkoem/CountHi2.java
@@ -0,0 +1,17 @@
+package pl.xkoem;
+
+public class CountHi2 {
+    public int countHi2(String str) {
+        if (str.length() < 2) {
+            return 0;
+        }
+        if (str.charAt(1) == 'h' && str.charAt(2) == 'i' && str.charAt(0) != 'x') {
+            return 1 + countHi2(str.substring(3));
+        } else if (str.charAt(1) == 'h' && str.charAt(2) == 'i' && str.charAt(0) == 'x') {
+            return countHi2(str.substring(3));
+        } else if (str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+            return 1 + countHi2(str.substring(1));
+        }
+        return countHi2(str.substring(1));
+    }
+}
