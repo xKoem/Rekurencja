c44a716cf5c4e8b8743200c7e2764cd47076d549
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 11:42:13 +0200

    changeXY

---
+++ src/main/java/pl/xkoem/ChangeXY.java
@@ -0,0 +1,18 @@
+package pl.xkoem;
+
+public class ChangeXY {
+    public String changeXY(String str) {
+        if (str.length() == 0) {
+            return "";
+        }
+        char actualChar = str.charAt(str.length()-1);
+
+        if (actualChar == 'x') {
+            actualChar = 'y';
+        }
+
+        str = changeXY(str.substring(0, str.length()-1));
+        str+= actualChar;
+        return str;
+    }
+}
