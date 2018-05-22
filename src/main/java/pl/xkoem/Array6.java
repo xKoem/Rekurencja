375c9b643fa7455d22d0767843504b6fb15b95c6
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-22 12:08:14 +0200

    array6

---
+++ src/main/java/pl/xkoem/Array6.java
@@ -0,0 +1,13 @@
+package pl.xkoem;
+
+public class Array6 {
+    public boolean array6(int[] nums, int index) {
+        if (index == nums.length) {
+            return false;
+        }
+        if (nums[index] == 6) {
+            return true;
+        }
+        return array6(nums, index+1);
+    }
+}
