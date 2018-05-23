f1997fe2b96735df55b16571cd628b746e27fef2
Bartlomiej Kosmider <bartlomiej_kosmider@epam.com>
2018-05-23 14:28:33 +0200

    strCount

---
+++ src/main/java/pl/xkoem/recursion1/StrCount.java
@@ -0,0 +1,14 @@
+package pl.xkoem.recursion1;
+
+public class StrCount {
+    public int strCount(String str, String sub) {
+        if (str.length() < sub.length()) {
+            return 0;
+        }
+        if (str.substring(0, sub.length()).equals(sub)) {
+            return 1 + strCount(str.substring(sub.length()), sub);
+        }
+        return strCount(str.substring(1), sub);
+    }
+
+}
